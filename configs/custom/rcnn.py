9531870c575dab5fbc9a51d2d027318c98ff7cea
Nan Ya <aerysnan@gmail.com>
2022-02-16 13:49:46 +0800

    restore config, update readme and bugfix

---
+++ configs/custom/rcnn.py
@@ -0,0 +1,26 @@
+_base_ = '../faster_rcnn_r101_fpn_1x_coco.py'
+# model settings
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(960, 540),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+model = dict(
+    test_cfg=dict(
+        rcnn=dict(
+            score_thr=0.1,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100)
+    ))
